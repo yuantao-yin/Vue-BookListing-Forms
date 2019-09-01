3b932e1be2c603621e69e759f0cc5068c8536c80
YuantaoYin <yuantao.yin@delta.com>
2019-09-01 00:29:18 -0400

    update

--- src/components/BookForm.vue
+++ src/components/BookForm.vue
@@ -1,7 +1,24 @@
 <template>
-  <form v-on:submit.prevent="bookSubmit(bookTitle, bookAuthor)" class="" action="#" method="post">
-    <input v-model="bookTitle" type="text" name="title" value="" placeholder="Book Title">
-    <input v-model="bookAuthor" type="text" name="author" value="" placeholder="Book Author">
+  <form v-on:submit.prevent="bookSubmit(bookData)" class="" action="#" method="post">
+    <input v-model="bookData.bookTitle" type="text" name="title" value="" placeholder="Book Title">
+    <input v-model="bookData.bookAuthor" type="text" name="author" value="" placeholder="Book Author">
+    <div>
+      <label for="finishedReading">
+        Finished Reading
+        <input type="checkbox" v-model="bookData.finishedReading">
+      </label>
+      
+    </div>
+    <div>
+      <label for="borrowed">
+        borrowed
+        <input type="radio" value="borrowed" v-model="bookData.ownership">
+      </label>
+      <label for="bought">
+        bought
+        <input type="radio" value="bought" v-model="bookData.ownership">
+      </label>
+    </div>
     <button type="submit" name="button">Add Book</button>
   </form>
 </template>
@@ -12,13 +29,18 @@ export default {
   props: ["books"],
   data() {
     return {
-      bookTitle: "",
-      bookAuthor: ""
+      bookData: {
+        bookTitle: "",
+        bookAuthor: "",
+        finishedReading: false,
+        ownership:[]
+      }
+      
     };
   },
   methods: {
-    bookSubmit(bookTitle, bookAuthor) {
-      this.$emit("addBook", bookTitle, bookAuthor);
+    bookSubmit(bookData) {
+      this.$emit("addBook", bookData);
     }
   }
 };
